6a0b6e3ac807f8384ba62aed13635e97355ebc42
hanana1253 <hanana1253@gmail.com>
2021-07-23 16:36:18 +0900

    feat: Create fibonachicken file
    To practice git collaboration but i'm doing it alone as a class manager..
    I'm not lonely...

---
+++ git/fibonachicken.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# -*- coding:utf-8 -*-
+num = int(input('몇 명을 먹이고자 하는고?'))
+
+if num <= 2:
+    result = '당연히 {num}, 1인 1닭의 미덕을 지키거라'.format(num)
+else:
+    result = '아직 만드는 중'
+
+print(result)
+
