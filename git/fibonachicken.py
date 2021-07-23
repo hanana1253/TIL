0a6751f1e6a268a629883e81f0f9a744fcf588b6
hanana1253 <hanana1253@gmail.com>
2021-07-23 16:39:33 +0900

    feat: Fixed string formatting error
    It's always confusing

--- git/fibonachicken.py
+++ git/fibonachicken.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 # -*- coding:utf-8 -*-
+
+
 num = int(input('몇 명을 먹이고자 하는고?'))
 
+
 if num <= 2:
-    result = '당연히 {num}, 1인 1닭의 미덕을 지키거라'.format(num)
+    result = '당연히 {}마리, 1인 1닭의 미덕을 지키거라'.format(num)
 else:
     result = '아직 만드는 중'
 
